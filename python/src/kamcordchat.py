b437d5c8364f9b5aaacd252a063f5a5970e8ae73
DazBoot <Lekgold@gmail.com>
2016-01-31 21:42:44 -0500

    Refactoring the chat class to be organized

--- python/src/kamcordchat.py
+++ python/src/kamcordchat.py
@@ -6,6 +6,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from config import *
 
+def login( driver, username, password ):
+    usernameInput = driver.find_elements_by_name( "username" )[ 1 ]
+    usernameInput.send_keys( USERNAME )
+    
+    passwordInput = driver.find_elements_by_name( "password" )[ 1 ]
+    passwordInput.send_keys( PASSWORD )
+    passwordInput.submit()
+    
 def sendMessage( driver, msg ):
     commentInput = driver.find_element_by_class_name( "live-comment-input" )
     commentInput.send_keys( msg )
@@ -28,17 +36,10 @@ if __name__ == "__main__":
     
     time.sleep( 1 )
     
-    #Enter the username and password
-    usernameInput = driver.find_elements_by_name( "username" )[ 1 ]
-    usernameInput.send_keys( USERNAME )
-    
-    passwordInput = driver.find_elements_by_name( "password" )[ 1 ]
-    passwordInput.send_keys( PASSWORD )
-    passwordInput.submit()
+    login( driver, USERNAME, PASSWORD )
     
     time.sleep( 1 )
     
     #Send a dummy message to trigger the login prompt
-    inputElement = driver.find_element_by_class_name("live-comment-input")
-    inputElement.send_keys('This is a test message from DazBot!')
+    sendMessage( driver, "This is a test message from DazBoot!" )
     inputElement.submit()
